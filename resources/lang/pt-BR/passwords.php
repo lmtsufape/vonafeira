a2ac664c80abde257ff0f6432cc24fb4a7e7ce7d
AdelinoLSN <adelinolsn@gmail.com>
2018-12-13 17:42:59 -0300

    Adicionados arquivos de linguagem de erros para pt-BR (português-brasileiro)

---
+++ resources/lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas precisam conter ao menos seis caracteres e correspoderem à senha confimada.',
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Lhe enviamos um e-mail com link para a redefinição de senha.',
+    'token' => 'Esse token de redefinição de senha é inválido.',
+    'user' => "Não foi possível encontrar um usuário com esse endereço de e-mail.",
+
+];
