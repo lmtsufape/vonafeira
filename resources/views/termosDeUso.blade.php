cb59c4a6f36191fcb459d0ad660d161f212c1f1d
AdelinoLSN <adelinolsn@gmail.com>
2019-01-11 13:53:19 -0300

    Melhorias nos Termos de Uso

--- resources/views/termosDeUso.blade.php
+++ resources/views/termosDeUso.blade.php
@@ -9,24 +9,43 @@
 </head>
 <body>
     <h3>Feira Solidária - Temos de Uso</h3>
-    <h5>Última atualização: 23 de Dezembro de 2018</h5>
+    <h5>Última atualização: 11 de Janeiro de 2019</h5>
     
     <div class="container">
-        Esse termo afirma um contrato com o usuário, permitindo assim a ruptura 
-        do mesmo, isto é, exclusão de cadastro, ou punições mais leves 
-        pelo administrador se o mesmo entender que alguma das seguintes regras 
-        foi violada pela parte contratante (usuário):
-        <div class="container">
-            <ul>
-                <li>
-                    Venda de qualquer produto de natureza ilícita (drogas).
-                </li>
-                <li>
-                    Não comparecimento para entrega/retirada de produtos 
-                    comprados/vendidos sem prévio aviso.
-                </li>
-            </ul>
-        </div>
+        <p style="text-indent: 40px" align="justify">
+            Estes Termos são um contrato aceito por VOCÊ no momento em que 
+            VOCÊ faz uso do nosso sistema, independentemente de qualquer 
+            outra forma de aceitação. VOCÊ deve estar ciente de que algumas 
+            das ferramentas oferecidas estão sujeitas a condições particulares 
+            que regem o relacionamento dos envolvidos.
+        </p>
+        <p style="text-indent: 40px" align="justify">
+            No uso de nossas ferramentas, VOCÊ deve obedecer a legislação 
+            vigente, a moral, a ordem pública e estes Termos e demais condições 
+            particulares a cada caso, não podendo praticar nenhuma conduta, 
+            incluindo a publicação de anúncios, que sejam contrários a eles.
+        </p>
+        <p style="text-indent: 40px" align="justify">
+            Os desenvolvedores e equipe geral não se responsabilizam por danos 
+            decorrentes dos negócios conduzidos por terceiros, USUÁRIOS ou não, 
+            pois sua atuação se limita a disponibilizar o espaço para oferta de 
+            produtos e serviços diretamente pelos USUÁRIOS anunciantes. Por esse 
+            mesmo motivo, não nos responsabilizam pela conduta dos USUÁRIOS, por 
+            conteúdos, produtos e serviços anunciados ou insucesso das negociações.
+        </p>
+        <p style="text-indent: 40px" align="justify">
+            VOCÊ é integralmente responsável por todos os atos praticados por VOCÊ. 
+            Da mesma forma, VOCÊ também é responsável pela veracidade das informações 
+            fornecidas, seja para o registro da conta, seja para a condução de suas 
+            negociações, e se atentará às Regras para produtos passíveis de venda que 
+            integram estes Termos que podem ser acessadas AQUI.
+        </p>
+        <p style="text-indent: 40px" align="justify">
+            A "Vo Na Feira" se reserva o direito de recusar, bloquear, inabilitar ou 
+            excluir, a seu exclusivo critério, USUÁRIOS que descumpram qualquer uma 
+            das disposições presentes nestes Termos e Condições ou outros instrumentos 
+            que os integram.
+        </p>
     </div>
 
 </body>
