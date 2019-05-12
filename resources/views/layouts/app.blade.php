f7af80701e386f91294c6d1791150c67152ca484
EbersonSantos <eberson.santos1@gmail.com>
2019-05-12 13:24:41 -0300

    correção footer fixo no fim da página

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,13 +122,6 @@
           padding-bottom: 2.5rem;    /* Footer height */
         }
 
-        /* #footer {
-          position: absolute;
-          bottom: 0;
-          width: 100%;
-          height: 2.5rem;
-        } */
-
         @media (max-width: 1024px) {
           #barra-logos{display: none;}
           .btn-toggle{display: block;}
